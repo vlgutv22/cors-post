d57cecbbba3d6b76ffd43f700ce1d460a48567d7
Vladimir Gutorov <vlgutv@gmail.com>
2019-08-17 11:49:50 +0300

    Add env port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const curl = new (require('curl-request'))();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.options('*', cors());
@@ -27,6 +28,6 @@ app.get('/', function(req, res) {
   }
 });
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function() {
+  console.log('Example app listening on port: ' + port);
 });
