04b69e06c474ad0aa0c998f8a9b44583ef6687ee
Vladimir Gutorov <vlgutv@gmail.com>
2020-09-30 21:59:16 +0300

    Finalize proxy

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ app.get('/', function (req, res) {
     const body = JSON.parse(req.query.body);
 
     curl.request({ url, data: body }, function (err, data) {
-      console.log(err, data);
+      if (err === null) {
+        res.send(data);
+        res.status(200);
+      } else {
+        res.status(400);
+        res.send(err);
+      }
     });
   } else {
     res.status(400);
