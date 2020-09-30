902c56776ceab8c940c7fe1f578f365a5c8bc231
Vladimir Gutorov <vlgutv@gmail.com>
2020-09-30 21:59:43 +0300

    Cleanup code

--- index.js
+++ index.js
@@ -29,7 +29,3 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
   console.log('Example app listening on port: ' + port);
 });
-
-// curl 'https://webflow.com/api/v1/form/5f3ce7d38b0a42cbcba07bf6' \
-//   --data-raw 'name=Email+Form&source=https%3A%2F%2Fforager.ai%2F&test=false&fields%5BName+2%5D=Gutorov+Vladimir&fields%5BEmail+2%5D=vlgutv%40gmail.com&fields%5BField+3%5D=test&fields%5BField+4%5D=test.com&dolphin=false' \
-//   --compressed
