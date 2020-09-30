4cde7da28421e009161636d232f3363b3d64e2b2
Vladimir Gutorov <vlgutv@gmail.com>
2020-09-30 21:35:51 +0300

    Change lib

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-var curl = require('curlrequest');
+const request = require('request');
 const port = process.env.PORT || 8080;
 
 app.use(cors());
@@ -10,11 +10,22 @@ app.get('/', function (req, res) {
   if (req.query.url && req.query.body) {
     const url = req.query.url;
     const body = JSON.parse(req.query.body);
-    const options = { method: 'POST', url, data: req.query.body };
+    const options = {
+      method: 'POST',
+      url: url,
+      body: body,
+      json: true, // JSON stringifies the body automatically
+    };
 
-    curl.request(options, function (err, data) {
-      console.log(err, data);
-    });
+    request(options)
+      .then(function (response) {
+        console.log(response);
+        // Handle success response data
+      })
+      .catch(function (err) {
+        console.log(response);
+        // Handle err response
+      });
   } else {
     res.status(400);
     res.send('Invalid request');
